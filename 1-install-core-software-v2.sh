ab3c44ad87272fd43422726b9b3dc3f9eaa1dbc2
lxgolovin <lxgolovin@gmail.com>
2019-09-25 13:54:23 +0300

    Remmina added to installation

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y curl dconf-cli dconf-editor dropbox evolution geany gear
 sudo apt-get install -y glances gparted grsync hardinfo inkscape meld
 sudo apt-get install -y openshot plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
 sudo apt-get install -y thunar vlc vnstat mc aptitude firefox firefox-locale-en firefox-locale-ru
+sudo apt-get install -y libzen0v5 libmediainfo0v5 libcrypto++6 libc-ares2 libtinyxml2-6
 
 # installation of zippers and unzippers
 sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
---
+++ more_software/install-remmina-v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# forked by
+# Author 	: 	Alexander Golovin
+# Website 	: 	https://eshkincode.ru
+##################################################################################################################
+
+sudo snap install remmina
+sudo snap connect remmina:avahi-observe :avahi-observe
+sudo snap connect remmina:cups-control :cups-control
+sudo snap connect remmina:mount-observe :mount-observe
+sudo snap connect remmina:password-manager-service :password-manager-service
+
+echo "################################################################"
+echo "###################     Remmina installed    ###################"
+echo "################################################################"
