732e7ae284366fa865c3d5ceba4b2254ada9eb8b
lxgolovin <lxgolovin@gmail.com>
2019-09-29 22:37:56 +0300

    Grub added

--- more_software/install-grub-customizer-v1.sh
+++ more_software/install-grub-customizer-v1.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# repo for grub-customizer
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-
-
-# getting new info of this new repo
-sudo apt-get -y update
-
-# installing
-sudo apt-get install -y grub-customizer
-
-echo "################################################################"
-echo "###################    T H E   E N D      ######################"
-echo "################################################################"
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update
+sudo apt install grub-customizer -y
