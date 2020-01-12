f70c04c2dfdd731aeeeaee1b5d25ce11b370c7fd
lxgolovin <lxgolovin@gmail.com>
2020-01-12 09:58:31 +0300

    project refactoring

--- install-virtual-box-v2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wget https://download.virtualbox.org/virtualbox/6.0.4/virtualbox-6.0_6.0.4-128413~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
-
-sudo apt-get install virtualbox -y
-
-echo "################################################################"
-echo "###################  VirtualBox Installed ######################"
-echo "################################################################"
