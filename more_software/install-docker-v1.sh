3aef626d5cb55955b6dee9393f025328e8b4ccb8
Alexander <lxgolovin@gmail.com>
2020-02-20 23:03:43 +0300

    docker install correction

--- more_software/install-docker-v1.sh
+++ more_software/install-docker-v1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
-sudo sh get-docker.sh
+sudo sh /tmp/get-docker.sh
 
 sudo groupadd docker
-sudo usermod -aG docker `id -un` 
+sudo usermod -aG docker `id -un`
 
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 #curl -fLo ~/.oh-my-zsh/plugins/docker/_docker https://raw.github.com/felixr/docker-zsh-completion/master/_docker
